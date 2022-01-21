d743c593360500134dab2e82c7f0d5308302022d
tianzhixiaoyan <2793886772@qq.com>
2022-01-21 21:12:06 +0800

    first coud

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int sort()//查找,找到返回，找不到返回-1
+{
+    int n=0;
+    pdput()
+}
+void pdput()//输入（rule）
+{
+    freopen("rule1.txt","r",stdin);
+    char rrootip[20],rgoalip[20];//IP地址（点分十进制）
+    long long rleft,rright;//端口
+    long long rlefttyp,rrighttyp;//协议号范围
+    while(scanf()==1)
+    {
+
+    }
+}
+void put()//输入（packet）
+{
+    long long prootip,pgoalip;//IP地址（十进制）
+    long long pleft,pright;//端口
+    long long ptyp;//协议号
+    while(scanf("%lld %lld %lld %lld %lld",&prootip,&pgoalip,&pleft,&pright,&ptyp)==1)//判断输入是否结束
+    {
+    }
+}
+
+int main()
+{
+    freopen("packet1.txt","r",stdin);
+    freopen("res.txt","w",stdout);
+    put();
+
+    return 0;
+}
