28b1f3710f8ea6eec2d99e81395464ed08ed9c8a
tianzhixiaoyan <2793886772@qq.com>
2022-01-23 19:14:32 +0800

    second code

--- main.cpp
+++ main.cpp
@@ -1,10 +1,37 @@
 #include<iostream>
 #include<cstdio>
 using namespace std;
+int transform(char a[32])//转变32位
+{
+    for(i=0;i<8;i++)
+{
+if(a[i]=='1')
+b[0]+=erdn[i];
+}
+for(i=8;i<16;i++ )
+{
+if(a[i]=='1')
+b[1]+=erdn[i-8];
+}
+for(i=16;i<24;i++)
+{
+if(a[i]=='1')
+b[2]+=erdn[i-16];
+}
+for(i=24;i<32;i++)
+{
+if(a[i]=='1')
+b[3]+=erdn[i-24];
+}
+for(i=0;i<4;i++)
+{
+printf("%d",b[i]);
+if(i%1==0&&i<3)
+printf(".");
+}
+}
 int sort()//查找,找到返回，找不到返回-1
 {
-    int n=0;
-    pdput()
 }
 void pdput()//输入（rule）
 {
@@ -12,25 +39,17 @@ void pdput()//输入（rule）
     char rrootip[20],rgoalip[20];//IP地址（点分十进制）
     long long rleft,rright;//端口
     long long rlefttyp,rrighttyp;//协议号范围
-    while(scanf()==1)
-    {
-
-    }
 }
 void put()//输入（packet）
 {
     long long prootip,pgoalip;//IP地址（十进制）
     long long pleft,pright;//端口
     long long ptyp;//协议号
-    while(scanf("%lld %lld %lld %lld %lld",&prootip,&pgoalip,&pleft,&pright,&ptyp)==1)//判断输入是否结束
-    {
-    }
 }
 
 int main()
 {
-    freopen("packet1.txt","r",stdin);
-    freopen("res.txt","w",stdout);
+    
     put();
 
     return 0;
